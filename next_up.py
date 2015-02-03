69087d676bd5ec93cf547273340dd8c5e66b1797
James Tunnicliffe <james.tunnicliffe@nanosheep.org>
2015-02-03 13:23:39 +0000

    Look up bugs on LP

--- next_up.py
+++ next_up.py
@@ -6,6 +6,9 @@ import settings
 import pymongo
 import os
 from pprint import pprint
+from launchpadlib.launchpad import Launchpad
+import re
+
 
 BASE_DIR = os.path.dirname(__file__)
 app = Flask(__name__)
@@ -13,6 +16,9 @@ client = pymongo.MongoClient()
 db = client['next_up']
 #db.drop_collection('web_cache')
 web_cache = db['web_cache']
+lp_bug_cache = db['lp_bug_cache']
+bug_watch = db['bug_watch']
+
 
 class CacheEntry():
     def __init__(self, db, url):
@@ -53,38 +59,71 @@ def get_cards():
     return get_url(url, auth)
 
 def get_bugs():
-    cachedir = os.path.join(settings.PROJECT_ROOT, ".launchpadlib/cache/")
+    data = {}
+    cachedir = os.path.join(BASE_DIR, ".launchpadlib/cache/")
     launchpad = Launchpad.login_anonymously('just testing', 'production', cachedir)
-    me = launchpad.people[settings.LP_USER]
-    self.data["lp_bugs"] = []
-    for bug in me.searchTasks(assignee=me):
-        self.data["lp_bugs"].append({
-            "title": re.search('"(.*)"', bug.title).group(1),
-            "url": bug.web_link,
-            "target": bug.bug_target_display_name,
-            "importance": bug.importance,
-            "status": bug.status
-        })
+    if False:
+        me = launchpad.people[settings.LP_USER]
+        data["lp_bugs"] = []
+        for bug in me.searchTasks(assignee=me):
+            data["lp_bugs"].append({
+                "title": re.search('"(.*)"', bug.title).group(1),
+                "url": bug.web_link,
+                "target": bug.bug_target_display_name,
+                "importance": bug.importance,
+                "status": bug.status
+            })
 
-    bug_prio = {
-        "Undecided": 0,
-        "Critical": 1,
-        "High": 2,
-        "Medium": 3,
-        "Low": 4,
-        "Wishlist": 5,
-    }
+        bug_prio = {
+            "Undecided": 0,
+            "Critical": 1,
+            "High": 2,
+            "Medium": 3,
+            "Low": 4,
+            "Wishlist": 5,
+        }
 
-    self.data["lp_bugs"] = sorted(self.data["lp_bugs"], key=lambda bug: bug_prio[bug["importance"]])
+        data["lp_bugs"] = sorted(data["lp_bugs"], key=lambda bug: bug_prio[bug["importance"]])
 
 
-    self.data["lp_bugs"] = [b for b in self.data["lp_bugs"] if
-                            bug_prio[b["importance"]] <=
-                            bug_prio[settings.MIN_BUG_PRIORITY]]
+        data["lp_bugs"] = [b for b in data["lp_bugs"] if
+                                bug_prio[b["importance"]] <=
+                                bug_prio[settings.MIN_BUG_PRIORITY]]
 
-    self.data["lp_bugs"] = [b for b in self.data["lp_bugs"] if
-                            b["target"] not in
-                            settings.IGNORE_PROJECTS]
+        data["lp_bugs"] = [b for b in data["lp_bugs"] if
+                                b["target"] not in
+                                settings.IGNORE_PROJECTS]
+
+    # Get bugs I have asked next_up to notify me about
+
+    for bug in bug_watch.find():
+        if bug['backend'] == "lp":
+            lp_bug = launchpad.bugs[bug['id']]
+            updated = False
+            if(not bug.get('http_etag') or
+               lp_bug.http_etag != bug.get('http_etag')):
+                # New or updated
+                updated = True
+                bug['http_etag'] = lp_bug.http_etag
+                bug['name'] = lp_bug.title
+                bug['url'] = lp_bug.web_link
+                print lp_bug.self_link
+                if not bug.get('tasks'):
+                    bug['tasks'] = {}
+            for task in lp_bug.bug_tasks:
+                for attr in dir(task):
+                    print attr, getattr(task, attr)
+                if(not task.self_link in bug['tasks'] or
+                   bug['tasks'][task.self_link]['http_etag'] != task.http_etag):
+                    updated = True
+                    bug['tasks'][task.self_link] = {
+                        'status': task.status,
+                        'http_etag': task.http_etag,
+                    }
+            if updated:
+                bug_watch.save(bug)
+
+    #return data["lp_bugs"]
 
 @app.route('/')
 def hello_world():
@@ -93,6 +132,35 @@ def hello_world():
 
 if __name__ == '__main__':
     c = json.loads(get_cards())
+    bug_watch.drop()
+    bug_watch.save({
+        'backend': 'lp',
+        'id': 1416006,
+    })
+
+    lp_bugs = get_bugs()
+
+    for bug in bug_watch.find():
+        pprint(bug)
+
+
+    exit(0)
+
+
+    bug = lp_bugs[0]
+    #print dir(bug)
+    pprint(bug)
+    print bug.date_last_updated
+    #print bug.target
+    print bug.web_link
+    print bug.title
+    #print bug.importance
+    #print bug.status
+    #for attr in dir(bug):
+        #print attr, getattr(bug, attr)
+    for task in bug.bug_tasks:
+        print task.status
+    exit()
     print "boo"
     #pprint(c['ReplyData'][0]['Lanes'][0]['Cards'][0])
     # Search ^^['AssignedUsers']['FullName'] == "James Tunnicliffe"
--- settings.py
+++ settings.py
@@ -1,2 +1,12 @@
 leankit_pass = "oC2s3zEQI2e8RPTkyosW"
 leankit_user = "james.tunnicliffe@canonical.com"
+LP_USER = "dooferlad"
+MIN_BUG_PRIORITY = "High"
+IGNORE_PROJECTS = ["Linaro Fetch Image"]
+ALLOWED_HOSTS = ["*"]
+IGNORE = {
+    "lp_bp": [
+        "https://blueprints.launchpad.net/linaro/+spec/fetch-image",
+        "https://blueprints.launchpad.net/linaro-offspring/+spec/fork-offspring"
+    ]
+}
